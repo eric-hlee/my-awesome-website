295e1d261d1b62fae16ab053d30bcba0405f7a95
Eric Lee <ericlee1995@hotmail.com>
2020-05-18 19:32:45 +0800

    Deploy and use MongoDB Atlas instead of local database

--- my-api/app.js
+++ my-api/app.js
@@ -1,64 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+var MongoClient = require("mongodb").MongoClient;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var aboutRouter = require('./routes/about');
-var resumeRouter = require('./routes/resume');
+var indexRouter = require("./routes/index");
+var aboutRouter = require("./routes/about");
+var resumeRouter = require("./routes/resume");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/about', aboutRouter);
-app.use('/resume', resumeRouter);
+app.use("/", indexRouter);
+app.use("/about", aboutRouter);
+app.use("/resume", resumeRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
- 
-// Connection URL
-const url = 'mongodb://localhost:27017';
- 
-// Database Name
-const dbName = 'eric';
- 
-// Use connect method to connect to the server
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
+
+const uri =
+  "mongodb+srv://ericlee:lcPBtSuBrg4k3M5b@cluster0-xandl.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  if (err) throw err;
+  console.log("Connected successfully to server.");
   client.close();
 });
 
--- my-api/routes/about.js
+++ my-api/routes/about.js
@@ -2,27 +2,20 @@ var express = require("express");
 var router = express.Router();
 var MongoClient = require("mongodb").MongoClient;
 
-// db url, collection name and db name
-const url = "mongodb://ericlee:mongo@localhost:27017";
-const dbName = "eric";
-const collName = "work_experience";
+const uri =
+	"mongodb+srv://ericlee:lcPBtSuBrg4k3M5b@cluster0-xandl.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 router.get("/", (req, res, next) => {
-	MongoClient.connect(url, function (err, client) {
+	client.connect((err) => {
 		if (err) throw err;
-		// get db
-		const db = client.db(dbName);
 
-		// get collection
-		const collection = db.collection(collName);
+		const collection = client.db("eric").collection("workExperience");
 
-		// find all documents in the collection
 		collection.find().toArray((err, result) => {
 			if (err) throw err;
 			res.send(result);
 		});
-
-		// close db client
 		client.close();
 	});
 });

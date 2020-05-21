0b1f06b7bbe31fb21abf14a0030d6c66582d1d4c
Eric Lee <ericlee1995@hotmail.com>
2020-05-21 21:31:18 +0800

    Install and fix eslint errors

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react'
 
-export default function Footer() {
-	return (
-		<footer className="footer">
-			<p className="icon-copyright">
-				2020 Eric Lee. Designed and built with{" "}
-				<span role="img" aria-label="love">
-					❤️
-				</span>
-				.
-			</p>
-			<a href="mailto:hsuan9528@gmail.com">
-				<span className="dot icon-mail-alt"></span>
-			</a>
-			<a href="http://www.linkedin.com/in/erichsuanlee">
-				<span className="dot icon-linkedin"></span>
-			</a>
-			<a href="http://github.com/ericlee1995">
-				<span className="dot icon-github"></span>
-			</a>
-		</footer>
-	);
+export default function Footer () {
+  return (
+    <footer className="footer">
+      <p className="icon-copyright">
+        2020 Eric Lee. Designed and built with{' '}
+        <span role="img" aria-label="love">
+          ❤️
+        </span>
+        .
+      </p>
+      <a href="mailto:hsuan9528@gmail.com">
+        <span className="dot icon-mail-alt"></span>
+      </a>
+      <a href="http://www.linkedin.com/in/erichsuanlee">
+        <span className="dot icon-linkedin"></span>
+      </a>
+      <a href="http://github.com/ericlee1995">
+        <span className="dot icon-github"></span>
+      </a>
+    </footer>
+  )
 }
